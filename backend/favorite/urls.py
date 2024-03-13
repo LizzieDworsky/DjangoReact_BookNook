24b820aa6b69856dc68efaa00b6d4b229e3f5f88
LizzieDworsky <amydworsky15@gmail.com>
2024-03-13 16:22:54 -0600

    set up boilerplate endpoints for favorite app

--- backend/favorite/urls.py
+++ backend/favorite/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.all_favorites),
+    path("<int:pk>/", views.favorite_details)
 ]
--- backend/favorite/views.py
+++ backend/favorite/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Favorite
+from .serializer import FavoriteSerializer
 
 # Create your views here.
+
+@api_view(["GET", "POST"])
+def all_favorites(request):
+    if request.method == "GET":
+        return Response("GET")
+    if request.method == "POST":
+        return Response("POST")
+    
+@api_view(["Delete"])
+def favorite_details(request, pk):
+    if request.method == "DELETE":
+        return Response("DELETE")
