72f24e927c2ddbfd1b491c380c9a6ef7e716c4a0
LizzieDworsky <amydworsky15@gmail.com>
2024-03-15 07:11:46 -0600

    updated review and favorite models to include user fk + set min-max values for reviews

--- backend/favorite/models.py
+++ backend/favorite/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Favorite(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE,
+        related_name = "favorites"
+    )
     book_id = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     thumbnail_url = models.TextField()
+
+    def __str__(self):
+        return f"{self.title} (Favorite of {self.user.username})"
--- backend/review/models.py
+++ backend/review/models.py
@@ -1,8 +1,23 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Review(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE,
+        related_name = "reviews"
+    )
     book_id = models.CharField(max_length=255)
     text = models.TextField()
-    rating = models.PositiveIntegerField()
+    rating = models.PositiveIntegerField(
+        validators = [
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ]
+    )
+
+    def __str__(self):
+        return f"Review by {self.user.username} for book {self.book_id} - Rating: {self.rating}"
