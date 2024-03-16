cc6b87a0b0130020a8fcb55cb873da4c38113168
LizzieDworsky <amydworsky15@gmail.com>
2024-03-16 09:17:11 -0600

    added custom UserSerializer and implemented on review serializer + added depth for ReviewSerializer

--- backend/review/serializer.py
+++ backend/review/serializer.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from .models import Review
 
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+
 class ReviewSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
     class Meta:
         model = Review
         fields = ['id', 'user', 'book_id', 'text', 'rating']
         read_only_fields = ['user', 'book_id']
+        depth = 1
