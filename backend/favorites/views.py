c8db00a2fbc62a4a8cf8365e56e695499dbebbea
LizzieDworsky <amydworsky15@gmail.com>
2024-03-16 10:33:44 -0600

    updated favorites endpoint to get all user's favorites

--- backend/favorites/views.py
+++ backend/favorites/views.py
@@ -1,4 +1,6 @@
-from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Favorite
@@ -7,13 +9,18 @@ from .serializer import FavoriteSerializer
 # Create your views here.
 
 @api_view(["GET", "POST"])
+@permission_classes([IsAuthenticated])
 def all_favorites(request):
     if request.method == "GET":
-        return Response("GET")
+        favorites = request.user.favorites.all()
+        serializer = FavoriteSerializer(favorites, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     if request.method == "POST":
+        
         return Response("POST")
     
 @api_view(["DELETE"])
+@permission_classes([IsAuthenticated])
 def favorite_details(request, pk):
     if request.method == "DELETE":
         return Response("DELETE")
