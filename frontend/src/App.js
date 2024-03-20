0d408430372689cb1b6c4238912736718e223ed7
LizzieDworsky <amydworsky15@gmail.com>
2024-03-20 11:32:43 -0600

    added logic for App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
+// Define the main layout component for the application
 function App() {
-    return <div className="App">Hello World</div>;
+    return (
+        <div className="App">
+            <Outlet />
+        </div>
+    );
 }
 
 export default App;
