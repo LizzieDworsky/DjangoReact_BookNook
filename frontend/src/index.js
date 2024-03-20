82c3a47400bb2bc0326169aee51eb561fa138c99
LizzieDworsky <amydworsky15@gmail.com>
2024-03-20 11:30:42 -0600

    update logic for index.js including adding router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from "./App";
 
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+
+import { AuthProvider } from "./utils/useAuth";
+import PrivateRoute from "./utils/PrivateRoute";
+
+// Define the application routes using React Router
+const router = createBrowserRouter([
+    {
+        path: "", // Base path
+        element: <App />, // Main layout component
+        // Nested components
+        children: [
+            {
+                path: "/",
+                element: <HomePage />,
+            },
+            {
+                path: "/register",
+                element: <RegisterPage />,
+            },
+            {
+                path: "/login",
+                element: <LoginPage />,
+            },
+        ],
+    },
+]);
+
+// Create a root container in the DOM
 const root = ReactDOM.createRoot(document.getElementById("root"));
+// Render the application wrapped in AuthProvider and RouterProvider
 root.render(
     <React.StrictMode>
-        <App />
+        <AuthProvider>
+            <RouterProvider router={router} />
+        </AuthProvider>
     </React.StrictMode>
 );
