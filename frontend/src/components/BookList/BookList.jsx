a5d4ea264cdba85d90afa15757b3af2d572719f6
LizzieDworsky <amydworsky15@gmail.com>
2024-03-22 19:20:41 -0600

    Improved alt text for image + cleaned up the console

--- frontend/src/components/BookList/BookList.jsx
+++ frontend/src/components/BookList/BookList.jsx
@@ -2,13 +2,12 @@ import BookListItem from "./BookListItem";
 import "./BookList.css";
 
 const BookList = ({ books }) => {
-    console.log(books);
     return (
         books && (
             <div className="book-list-container">
                 <ul className="book-list">
                     {books.map((book) => (
-                        <BookListItem book={book} />
+                        <BookListItem book={book} key={book.id} />
                     ))}
                 </ul>
             </div>
--- frontend/src/components/BookList/BookListItem.jsx
+++ frontend/src/components/BookList/BookListItem.jsx
@@ -1,5 +1,4 @@
 const BookListItem = ({ book }) => {
-    console.log(book.volumeInfo.authors);
     const placeHolderImg = "/images/StylizedBookImage.webp";
     const thumbnailUrl =
         book.volumeInfo.imageLinks?.smallThumbnail || placeHolderImg;
@@ -7,11 +6,14 @@ const BookListItem = ({ book }) => {
     return (
         book && (
             <li className="book-list-item">
-                <img src={thumbnailUrl} alt="Thumbnail of the book." />
+                <img
+                    src={thumbnailUrl}
+                    alt={`Cover for ${book.volumeInfo.title} book.`}
+                />
                 <h4>{book.volumeInfo.title}</h4>
                 <p>Author(s):</p>
                 {book.volumeInfo.authors.map((author) => (
-                    <p>{author}</p>
+                    <p key={author}>{author}</p>
                 ))}
             </li>
         )
