67f133f977aab2be554eccfbf449947973f3f0f9
LizzieDworsky <amydworsky15@gmail.com>
2024-04-02 14:35:16 -0600

    BookInfo component: added author implementation and condition checks for missing data

--- frontend/src/components/BookDetails/BookInfo.jsx
+++ frontend/src/components/BookDetails/BookInfo.jsx
@@ -1,11 +1,20 @@
 const BookInfo = ({ bookInfo }) => {
+    if (!bookInfo) return null;
+
     console.log(bookInfo);
     const placeHolderImg = "/images/StylizedBookImage.webp";
     const thumbnailUrl = bookInfo.imageLinks?.thumbnail || placeHolderImg;
+
+    const authors = bookInfo.authors?.map((author) => (
+        <li key={author}>{author}</li>
+    )) || <li>Unknown</li>;
+
     return (
         <div>
             <img src={thumbnailUrl} alt={`Cover for book.`} />
             <h2>{bookInfo.title}</h2>
+            <h3>Author(s)</h3>
+            <ul>{authors}</ul>
         </div>
     );
 };
