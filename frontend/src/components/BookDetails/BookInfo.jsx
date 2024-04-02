2ae08ad06527ff7ff015256ecf0fd742eb91f014
LizzieDworsky <amydworsky15@gmail.com>
2024-04-02 14:59:23 -0600

    BookInfo component: added description and categories

--- frontend/src/components/BookDetails/BookInfo.jsx
+++ frontend/src/components/BookDetails/BookInfo.jsx
@@ -5,9 +5,13 @@ const BookInfo = ({ bookInfo }) => {
     const placeHolderImg = "/images/StylizedBookImage.webp";
     const thumbnailUrl = bookInfo.imageLinks?.thumbnail || placeHolderImg;
 
-    const authors = bookInfo.authors?.map((author) => (
-        <li key={author}>{author}</li>
+    const authors = bookInfo.authors?.map((author, index) => (
+        <li key={`${author}${index}`}>{author}</li>
     )) || <li>Unknown</li>;
+    const categories =
+        bookInfo.categories?.map((category, index) => (
+            <li key={index}>{category}</li>
+        )) || null;
 
     return (
         <div>
@@ -15,6 +19,9 @@ const BookInfo = ({ bookInfo }) => {
             <h2>{bookInfo.title}</h2>
             <h3>Author(s)</h3>
             <ul>{authors}</ul>
+            <div dangerouslySetInnerHTML={{ __html: bookInfo.description }} />
+            <h3>Genres</h3>
+            <ul>{categories}</ul>
         </div>
     );
 };
