899fb760c3a3fcf61e1923beac998eb20ae4531e
LizzieDworsky <amydworsky15@gmail.com>
2024-04-03 14:58:28 -0600

    FavoriteButton component: added a get request for favorite object if user is logged in + added conditions for toggleFav

--- frontend/src/components/BookDetails/FavoriteButton.jsx
+++ frontend/src/components/BookDetails/FavoriteButton.jsx
@@ -1,14 +1,36 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useAuth } from "../../utils/useAuth";
 import { FaHeart } from "react-icons/fa";
 
 const FavoriteButton = ({ favBookInfo }) => {
     const [favState, setFavState] = useState(favBookInfo.isFav);
-    // const [favoriteObj, setFavoriteObj] = useState(null);
+    const [favoriteObj, setFavoriteObj] = useState(null);
+    const { isAuthenticated } = useAuth();
+    const token = localStorage.getItem("token");
 
     const toggleFav = () => {
-        const token = localStorage.getItem("token");
-        addFav(token);
+        if (isAuthenticated) {
+            favState ? deleteFav(token) : addFav(token);
+        }
+    };
+    useEffect(() => {
+        if (isAuthenticated) {
+            getFav(token);
+        }
+    }, []);
+    const getFav = async (token) => {
+        try {
+            const response = await axios.get(
+                `http://localhost:8000/api/favorites/${favBookInfo.bookId}/`,
+                { headers: { Authorization: `Bearer ${token}` } }
+            );
+            if (response.status === 200) {
+                setFavoriteObj(response.data);
+            }
+        } catch (error) {
+            console.error(error);
+        }
     };
     const addFav = async (token) => {
         try {
@@ -22,27 +44,27 @@ const FavoriteButton = ({ favBookInfo }) => {
             );
             if (response.status === 201) {
                 setFavState(!favState);
-                // setFavoriteObj(response.data);
-                // console.log(response.data);
+                setFavoriteObj(response.data);
             }
         } catch (error) {
             console.error(error);
         }
     };
-    // const deleteFav = async (token) => {
-    //     try {
-    //         const response = await axios.delete(
-    //             `http://localhost:8000/api/favorites/primaryKey/`,
-    //             { headers: { Authorization: "Bearer " + token } }
-    //         );
-    //         if (response.status === 204) {
-    //             setFavState(!favState);
-    //             setFavoriteObj(null)
-    //         }
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // };
+    const deleteFav = async (token) => {
+        console.log("testing");
+        // try {
+        //     const response = await axios.delete(
+        //         `http://localhost:8000/api/favorites/primaryKey/`,
+        //         { headers: { Authorization: "Bearer " + token } }
+        //     );
+        //     if (response.status === 204) {
+        //         setFavState(!favState);
+        //         setFavoriteObj(null)
+        //     }
+        // } catch (error) {
+        //     console.error(error);
+        // }
+    };
     return (
         <div>
             <FaHeart
