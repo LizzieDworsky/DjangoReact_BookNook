b9a2d4b451ed0825d2cb4f1a72bdae439d993e5e
LizzieDworsky <amydworsky15@gmail.com>
2024-04-04 11:05:27 -0600

    Review component: added data for display

--- frontend/src/components/Reviews/Review.jsx
+++ frontend/src/components/Reviews/Review.jsx
@@ -5,7 +5,9 @@ const Review = ({ review }) => {
 
     return (
         <div>
+            <p className="review-username">{review.user.username}</p>
             <RatingDisplay ratingValue={review.rating} />
+            <p className="review-text">{review.text}</p>
         </div>
     );
 };
