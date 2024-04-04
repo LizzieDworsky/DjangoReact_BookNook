8aa4f568ae6483be86687432b926ab9653b3ed46
LizzieDworsky <amydworsky15@gmail.com>
2024-04-04 13:32:53 -0600

    NewReviewsForm: implemented form logic to build object for request

--- frontend/src/components/Reviews/NewReviewsForm.jsx
+++ frontend/src/components/Reviews/NewReviewsForm.jsx
@@ -1,12 +1,41 @@
+import { useState } from "react";
 import RatingInteractive from "./RatingInteractive";
 
 const NewReviewsForm = ({}) => {
+    const [review, setReview] = useState({
+        text: "",
+        rating: 0,
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setReview({ ...review, [name]: value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(review);
+    };
+
     return (
         <div>
-            <div>NewReviewsForm</div>
-            <RatingInteractive
-                onRatingSelected={(rating) => console.log(rating)}
-            />
+            <h2>What do you think?</h2>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <RatingInteractive
+                    onRatingSelected={(rating) =>
+                        setReview({ ...review, ["rating"]: rating })
+                    }
+                />
+                <label htmlFor="review-text">Tell us about it.</label>
+                <input
+                    type="text"
+                    id="review-text"
+                    name="text"
+                    value={review.text}
+                    onChange={(e) => handleChange(e)}
+                />
+                <button>Submit</button>
+            </form>
         </div>
     );
 };
