23e617376197a022a71baef4008dcbc9de8e73eb
LizzieDworsky <amydworsky15@gmail.com>
2024-04-10 08:39:42 -0600

    ReviewSection: set up some conditional rendering based on if user is logged in and if there are existing reviews

--- frontend/src/components/Reviews/ReviewsSection.jsx
+++ frontend/src/components/Reviews/ReviewsSection.jsx
@@ -1,12 +1,28 @@
 import NewReviewsForm from "./NewReviewsForm";
 import ReviewsList from "./ReviewsList";
+import { useAuth } from "../../utils/useAuth";
 
 const Reviews = ({ appData }) => {
-    console.log(appData);
+    const { isAuthenticated } = useAuth();
+
+    const reviewFormSection = isAuthenticated ? (
+        <NewReviewsForm />
+    ) : (
+        <div>Log in to leave a review.</div>
+    );
+
+    const reviewListSection =
+        appData.reviews.length > 0 ? (
+            <ReviewsList reviewsList={appData.reviews} />
+        ) : (
+            <div>Be the first to leave a review.</div>
+        );
+    console.log(reviewListSection);
+
     return (
         <div>
-            <div>ReviewsSection</div> <NewReviewsForm />
-            <ReviewsList reviewsList={appData.reviews} />
+            {reviewFormSection}
+            {reviewListSection}
         </div>
     );
 };
