38773e191f5f5292f87b7e8d0419f0476cec336a
LizzieDworsky <amydworsky15@gmail.com>
2024-04-10 09:03:28 -0600

    BookDetailsPage: extracted axios calls to Google books API and application's backend into separate functions

--- frontend/src/pages/BookDetailsPage.jsx
+++ frontend/src/pages/BookDetailsPage.jsx
@@ -10,20 +10,36 @@ export async function getBookDetailsLoader({ params }) {
 
 async function getBooksDetailsSearch(bookId) {
     try {
-        const response1 = await axios.get(
-            `https://www.googleapis.com/books/v1/volumes/${bookId}`
-        );
-
-        const token = localStorage.getItem("token");
-        const headers = token ? { Authorization: "Bearer " + token } : {};
-        const response2 = await axios.get(
-            `http://localhost:8000/api/book_details/${bookId}/`,
-            { headers }
-        );
+        const response1 = await getBookInfo(bookId);
+        const response2 = await getAppData(bookId);
         return { bookInfo: response1.data.volumeInfo, appData: response2.data };
     } catch (error) {
         console.error(error);
         return [];
+    }
+}
+
+async function getBookInfo(bookId) {
+    try {
+        const response = await axios.get(
+            `https://www.googleapis.com/books/v1/volumes/${bookId}`
+        );
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function getAppData(bookId) {
+    try {
+        const token = localStorage.getItem("token");
+        const headers = token ? { Authorization: "Bearer " + token } : {};
+        const response = await axios.get(
+            `http://localhost:8000/api/book_details/${bookId}/`,
+            { headers }
+        );
+        return response;
+    } catch (error) {
+        console.log(error);
     }
 }
 
