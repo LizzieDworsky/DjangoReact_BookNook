af819b1487a0211c358483b933831c263a75d87b
LizzieDworsky <amydworsky15@gmail.com>
2024-04-21 10:44:48 -0600

    Implemented a hover effect for interactive star rating

--- frontend/src/components/Reviews/RatingInteractive.jsx
+++ frontend/src/components/Reviews/RatingInteractive.jsx
@@ -3,16 +3,31 @@ import { FaStar } from "react-icons/fa";
 
 const RatingInteractive = ({ onRatingSelected, rating, className }) => {
     const [selectedRating, setSelectedRating] = useState(rating);
+    const [hoverRating, setHoverRating] = useState(0);
 
     useEffect(() => {
         setSelectedRating(rating);
     }, [rating]);
 
+    const handleMouseEnter = (index) => {
+        setHoverRating(index);
+    };
+
+    const handleMouseLeave = () => {
+        setHoverRating(0);
+    };
+
     const displayStarRating = () => {
         return [1, 2, 3, 4, 5].map((index) => (
             <FaStar
                 key={index}
-                color={index <= selectedRating ? "#ffc107" : "#e4e5e9"}
+                className={
+                    index <= (hoverRating || selectedRating)
+                        ? "star-filled"
+                        : "star-empty"
+                }
+                onMouseEnter={() => handleMouseEnter(index)}
+                onMouseLeave={handleMouseLeave}
                 onClick={() => {
                     setSelectedRating(index);
                     onRatingSelected(index);
